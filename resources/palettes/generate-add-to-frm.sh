4752615b03fd450bf4587639289e0d828332b86a
grymmjack <grymmjack@gmail.com>
2024-02-04 04:39:45 -0500

    Modified script to ignore generate shell script when processing

--- resources/palettes/generate-add-to-frm.sh
+++ resources/palettes/generate-add-to-frm.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 for dir in $(echo $(ls . | tr -d ' ' | sed -e "s/\.GPL//ig")); do 
-    echo "AddItem __UI_NewID, \"$(echo $dir | sed -e 's/(/ (/ig')\""
+    if [ "./${dir}" != "${0}" ]; then
+        echo "    AddItem __UI_NewID, \"$(echo $dir | sed -e 's/(/ (/ig')\""
+    fi
 done
